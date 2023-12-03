8d95fc412c6ac9389ee9c33a384e5f5e5f12de62
Justin Hallier <justin.hallier+git@gmail.com>
2023-12-03 14:56:36 +1300

    Set up for mac

--- private_dot_config/zsh/functions/general.zsh
+++ private_dot_config/zsh/functions/general.zsh
@@ -1,3 +1,12 @@
 function zsh_test() {
   echo "PONG"
 }
+
+# Git alias to get all commits from yesterday
+gitstandup() {
+  if [ -z "$(git log --all --pretty=format:'* %s' --no-merges --reverse --author=Justin --since=yesterday.midnight)" ]; then
+    git log --all --pretty=format:'* %s' --no-merges --reverse --author=Justin --since=last.friday.midnight;
+  else
+    git log --all --pretty=format:'* %s' --no-merges --reverse --author=Justin --since=yesterday.midnight;
+  fi
+}
