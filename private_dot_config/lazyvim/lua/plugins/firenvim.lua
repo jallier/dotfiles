8f1eda9a40f342dfb791b1ef2c765ef1e9462192
Justin Hallier <justin@petdirect.co.nz>
2025-07-21 13:39:51 +1200

    Update firenvim config
    This config should lazyload the plugin, as well as only taking over the
    input element when you hit the keyboard shortcut to do so instead of
    automatically

--- private_dot_config/lazyvim/lua/plugins/firenvim.lua
+++ private_dot_config/lazyvim/lua/plugins/firenvim.lua
@@ -1,4 +1,25 @@
+-- return {
+--   "glacambre/firenvim",
+--   build = ":call firenvim#install(0)",
+-- }
+
 return {
   "glacambre/firenvim",
-  build = ":call firenvim#install(0)",
+  cond = not not vim.g.started_by_firenvim,
+  build = function()
+    require("lazy").load({ plugins = "firenvim", wait = true })
+    vim.fn["firenvim#install"](0)
+  end,
+
+  -- configure FireNvim here:
+  config = function()
+    vim.g.firenvim_config = {
+      -- config values, like in my case:
+      localSettings = {
+        [".*"] = {
+          takeover = "never",
+        },
+      },
+    }
+  end,
 }
