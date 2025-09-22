a453c918d2c3252908f404c0340f9d4b6e317e61
Justin Hallier <justin@petdirect.co.nz>
2025-09-22 13:25:05 +1200

    Update lazyvim config
    Fix an issue with the go to reference lsp config no longer working.
    
    Update the package versions etc from lazyvim

--- private_dot_config/lazyvim/lua/plugins/lsp.lua
+++ private_dot_config/lazyvim/lua/plugins/lsp.lua
@@ -6,7 +6,7 @@ return {
     -- disable a keymap
     keys[#keys + 1] = { "gr", false }
     -- change a keymap
-    keys[#keys + 1] = { "gt", "lua vim.lsp.buf.references()", desc = "References", nowait = true }
+    keys[#keys + 1] = { "gt", vim.lsp.buf.references, desc = "References", nowait = true }
     -- add a keymap
     -- keys[#keys + 1] = { "gt", "<cmd>echo 'hello'<cr>" }
   end,
