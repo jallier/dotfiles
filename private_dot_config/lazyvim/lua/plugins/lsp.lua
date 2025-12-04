1e875637909361e9bc013dae7133bbf323617b83
Justin Hallier <justin@petdirect.co.nz>
2025-12-04 15:32:06 +1300

    Update lazyvim stuff for new version
    Also update jj config file

--- private_dot_config/lazyvim/lua/plugins/lsp.lua
+++ private_dot_config/lazyvim/lua/plugins/lsp.lua
@@ -1,13 +1,17 @@
 -- LSP keymaps
 return {
   "neovim/nvim-lspconfig",
-  opts = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- disable a keymap
-    keys[#keys + 1] = { "gr", false }
-    -- change a keymap
-    keys[#keys + 1] = { "gt", vim.lsp.buf.references, desc = "References", nowait = true }
-    -- add a keymap
-    -- keys[#keys + 1] = { "gt", "<cmd>echo 'hello'<cr>" }
-  end,
+  keys = {
+    { "gr", false },
+    { "gt", vim.lsp.buf.references, desc = "References", nowait = true },
+  },
+  -- opts = function()
+  --   local keys = require("lazyvim.plugins.lsp.keymaps").get()
+  --   -- disable a keymap
+  --   keys[#keys + 1] = { "gr", false }
+  --   -- change a keymap
+  --   keys[#keys + 1] = { "gt", vim.lsp.buf.references, desc = "References", nowait = true }
+  --   -- add a keymap
+  --   -- keys[#keys + 1] = { "gt", "<cmd>echo 'hello'<cr>" }
+  -- end,
 }
