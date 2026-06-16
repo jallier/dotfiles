e46d3ff7645c9347419f83ba31babda35e53e5c0
Justin Hallier <justin@petdirect.co.nz>
2026-06-16 14:20:52 +1200

    Add diffview nvim plugin

---
+++ private_dot_config/lazyvim/lua/plugins/diffview.lua
@@ -0,0 +1,13 @@
+return {
+  "dlyongemallo/diffview-plus.nvim",
+  version = "*",
+  -- optional: lazy-load on command
+  cmd = {
+    "DiffviewOpen",
+    "DiffviewToggle",
+    "DiffviewFileHistory",
+    "DiffviewMergeFiles",
+    "DiffviewDiffFiles",
+    "DiffviewLog",
+  },
+}
